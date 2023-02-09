ff21bf98afe55b10571e0f8d0d1699a43df32a64
Msamuelcyrilraj <samuelmuppasari@gmai.com>
2023-02-09 21:39:46 +0530

    second commit

---
+++ form-main/formvalidation.js
@@ -0,0 +1,44 @@
+function ValidationForm() {
+    let name = document.forms["RegForm"]["Name"];
+    let address= document.forms["RegForm"]["Address"];
+    let email = document.forms["RegForm"]["Email"];
+    let phoneNumber = document.forms["RegForm"]["Telephone"];
+    let select = document.forms["RegForm"]["Subject"];
+    let pass = document.forms["RegForm"]["Password"];
+    if(name.value == "") {
+      alert("Please enter your name.");
+      username.focus();
+      return false;
+    }
+    if(email.value == "") {
+      alert("Please enter a valid e-mail address.");
+      email.focus();
+      return false;
+    }
+    if(email.value.indexOf("@", 0) < 0) {
+      alert("Please enter a valid e-mail address.");
+      email.focus();
+      return false;
+    }
+    if(email.value.indexOf(".", 0) < 0) {
+      alert("Please enter a valid e-mail address.");
+      email.focus();
+      return false;
+    }
+    if(phoneNumber.value == "") {
+      alert("Please enter your telephone number.");
+      phoneNumber.focus();
+      return false;
+    }
+    if(pass.value == "") {
+      alert("Please enter your password");
+      pass.focus();
+      return false;
+    }
+    if(select.selectedIndex < 1) {
+      alert("Please enter your course.");
+      select.focus();
+      return false;
+    }
+    return true;
+  }
